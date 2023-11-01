7d3199536d42c91660d4a724e3d77f8220ad8a97
Phil Thompson <34678+PhilT@users.noreply.github.com>
2023-11-01 10:50:44 +0000

    Add a basic scene

--- Main.cs
+++ Main.cs
@@ -2,6 +2,6 @@ using Godot;
 
 public partial class Main : Node3D
 {
-	public override void _Ready() { Fs.Main.ready(); }
-	public override void _Process(double delta) { Fs.Main.run(delta); }
+	public override void _Ready() { Fs.Main.ready(this); }
+	public override void _Process(double delta) { Fs.Main.run(this, delta); }
 }
--- main.fs
+++ main.fs
@@ -1,12 +1,31 @@
 module Fs.Main
 open Godot
 
-let ready () =
-  GD.Print("READY")
-  // Create a camera
-  // Create a sun
-  // Create environment
+let ready (main: Node3D) =
+  let camera = new Camera3D()
+  camera.Position <- Vector3(0f, 3f, 52f)
+  camera.Current <- true
+  camera.RotateX(-0.25f)
+  main.AddChild(camera)
 
-let run delta =
-  GD.Print("RUN")
+  let floor = new MeshInstance3D()
+  let planeMesh = new PlaneMesh()
+  planeMesh.Size <- Vector2(100f, 100f)
+  floor.Mesh <- planeMesh
+  main.AddChild(floor)
 
+  let sun = new DirectionalLight3D()
+  sun.ShadowEnabled <- true
+  sun.Position <- Vector3(0f, 0f, 0f)
+  sun.Rotation <- Vector3(-1.0472f, 2.618f, 0f)
+  main.AddChild(sun)
+
+  let world = new WorldEnvironment()
+  let env = new Environment()
+  env.SdfgiEnabled <- true
+
+  world.Environment <- env
+  main.AddChild(world)
+
+let run (main: Node3D) delta =
+  ()
